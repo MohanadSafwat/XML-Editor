244cf34f34d58c785d7cd9925b9a6bb20a26f2c1
Mostafa-hemma <76970262+Mostafa-hemma@users.noreply.github.com>
2021-07-13 15:49:23 +0200

    minifying + Gui

---
+++ Minifying XML C++/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+string Minify(string x)
+{
+int y = 0;
+int z = 0;
+for(int i=0;i<x.length();i++)
+    {
+         if(x.find("<!--")!=-1)
+        {
+        y=x.find("!")-1;
+        z=y;
+        while(x[z]!='>')
+        {
+            z++;
+        }
+        x=x.erase(y,(z-y+1));
+        }
+
+    }
+int k = 0;
+        for(int i=0 ; i< x.length() ; i++)
+            {
+                if(x[i]=='>'&&x[i+1]==' ')
+                {
+                    k = i;
+                    while (x[k] != '<')
+                    {
+                        k++;
+                    }
+                    x = x.erase(i+1, (k - i-1));
+                }
+            }
+
+            cout<<y<<endl<<z<<endl<<k;
+    return x;
+
+}
+
+
+
+
+int main()
+{
+string x="";
+string xx;
+int y = 0;
+int z = 0;
+do
+{
+    getline(cin,xx) ;
+    x+=xx;
+
+  /*  if(x.find("<!--")!=-1)
+    {
+        y=x.find("!")-1;
+        z=y;
+        while(x[z]!='>')
+        {
+            z++;
+        }
+        x=x.erase(y,(z-y+1));
+    }*/
+
+}while(xx !="q");
+string zezo=Minify(x);
+    /* int k = 0;
+            for(int i=0 ; i< x.length() ; i++)
+            {
+                if(x[i]=='>'&&x[i+1]==' ')
+                {
+                    k = i;
+                    while (x[k] != '<')
+                    {
+                        k++;
+                    }
+                    x = x.erase(i+1, (k - i-1));
+                }
+            }*/
+//cout<<zezo;
+//cout<<endl<<i<<endl<<k;
+    return 0;
+}
---
+++ minifying XML/minifying XML/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace minifying_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x="";
+            int y = 0;
+           int z = 0;
+            Console.WriteLine("enter your paragraph");
+            do 
+            {
+                x += Console.ReadLine();
+
+                if (x.Contains("<!--"))
+                {
+                    y = x.IndexOf("!")-1;
+                    z = y;
+                    while (x[z]!='>') 
+                    {
+                        z++;
+                    }
+                    x=x.Remove(y, (z - y+1));
+                }
+              
+
+            } while (x!=null && x.Substring(x.Length - 1) != "q" );
+            Console.WriteLine(y + "  " + z);
+            int k = 0;
+            for(int i=0;i<x.Length;i++)
+            {
+                if(x[i]=='>'&&x[i+1]==' ')
+                {
+                    k = i;
+                    while (x[k] != '<')
+                    {
+                        k++;
+                    }
+                    x = x.Remove(i+1, (k - i-1));
+                }
+            }
+           // Console.WriteLine(x);
+
+
+        }
+    }
+}
